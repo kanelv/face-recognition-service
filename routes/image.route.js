d19af273184c4a3c55092b394034b1ef893ce864
CUONG Le Van <cuonglvrepvn@gmail.com>
2018-05-06 15:14:24 +0700

    add comment to image route

--- routes/image.route.js
+++ routes/image.route.js
@@ -1,7 +1,13 @@
 module.exports = (app) => {
     const image = require("../controllers/image.controller");
 
+    // to post image into server
+    // post: localhost:3000/images
     app.post('/images', image.create);
+    // to get idImage last
+    // get: localhost:3000/images/number
     app.get('/images/number', image.number);
+    // to get Image base64
+    // get: localhost:3000/images/2
     app.get('/images/:idUser', image.getImagebyIdUser)
 }

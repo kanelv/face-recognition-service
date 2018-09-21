d7f533f1161bd4f115bd7df7c9b312e8a2f800d9
CUONG LE <cuong.le@vinasaver.vn>
2018-09-21 08:09:52 +0700

    audit fix, database fix

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/myproject',
+    url: 'mongodb://localhost:27017/facerecognitionsys',
     urlreal: 'mongodb://root:eckCos8keppyoph@ds211440.mlab.com:11440/facerecognitionsys'
 }

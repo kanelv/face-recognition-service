99b8287811f86d03ba8c4feba96ec9e163b18b50
CUONG LE <cuong.le@vinasaver.vn>
2018-09-24 16:33:04 +0700

    fix style code user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+  {
     userid: String,
     fullname: String,
     email: String,
     class: String,
     address: String
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 module.exports = mongoose.model('User', UserSchema);
